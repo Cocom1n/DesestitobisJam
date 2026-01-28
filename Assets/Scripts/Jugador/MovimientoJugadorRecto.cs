11042324e00a07f1ec5e1e0ebc7ccbe6f3d25c21
IvoTab11 <ivoagusmitasil@gmail.com>
2026-01-28 18:58:07 -0300

    Se logró lanzar las bombuchas con una munición de 3

--- Assets/Scripts/Jugador/MovimientoJugadorRecto.cs
+++ Assets/Scripts/Jugador/MovimientoJugadorRecto.cs
@@ -6,8 +6,6 @@ public class MovimientoJugadorRecto : MonoBehaviour
     [SerializeField] private Transform camara;
     public Vector3 CalcularDireccion(Vector2 input)
 {
-    // Ahora el movimiento es relativo al mundo, no a la cámara
-    // input.y (W/S) mueve en Z, input.x (A/D) mueve en X
     Vector3 direccion = new Vector3(input.x, 0, input.y);
     
     return direccion.normalized * velocidad;
